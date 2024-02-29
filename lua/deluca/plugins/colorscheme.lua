58e7afabe2cf3492ea2f2f28f284e67373b7d8aa
GDPereira <gdelucapereira@gmail.com>
2024-02-29 15:21:58 -0300

    feat: change colorscheme

--- lua/deluca/plugins/colorscheme.lua
+++ lua/deluca/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "morhetz/gruvbox",
+    "joshdick/onedark.vim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme gruvbox]])
+      vim.cmd([[colorscheme onedark]])
     end,
   },
 }
