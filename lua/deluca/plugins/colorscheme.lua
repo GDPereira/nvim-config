da4d383e08bc94c7b7ea0ddf87d3a29785d3ac6f
GDPereira <gdelucapereira@gmail.com>
2024-02-29 15:13:17 -0300

    feat: add new plugins
    - Added chatgpt plugin;
    - Changed colorscheme to gruvbox;
    - Added diffview;

--- lua/deluca/plugins/colorscheme.lua
+++ lua/deluca/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "joshdick/onedark.vim",
+    "morhetz/gruvbox",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme onedark]])
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
 }
