2132adc5201b84b3e32408cd0bc4bebb13db8901
GDPereira <gdelucapereira@gmail.com>
2024-03-28 20:57:44 -0300

    feat: add new keybinds

--- lua/deluca/core/keymaps.lua
+++ lua/deluca/core/keymaps.lua
@@ -30,4 +30,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
--- keymap.set("n", "<leader>tkw", ":lua vim.cmd('tabdo tabclose')<CR>", { desc = "Close all tabs" })
+
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
