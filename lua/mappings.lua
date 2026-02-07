304645723e3fd11b676b6bfc3388fd18d04b4f93
GDPereira <gdelucapereira@gmail.com>
2026-02-07 00:30:29 -0300

    feat: added new plugin gitsigns
    - added new map for nohl

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
